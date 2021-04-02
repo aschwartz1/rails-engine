2263b7c4401a9f30d2175e2938573afbf642da61
Alex Schwartz <aschwartz@detaso.com>
2021-04-02 12:11:46 -0500

    Add through relationships to item

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,5 +4,7 @@ RSpec.describe Item, type: :model do
   describe 'relationships' do
     it { should belong_to :merchant }
     it { should have_many(:invoice_items).dependent(:destroy) }
+    it { should have_many(:invoices).through(:invoice_items) }
+    it { should have_many(:customers).through(:invoices) }
   end
 end
