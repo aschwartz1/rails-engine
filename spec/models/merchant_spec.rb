eac62bc9ac4c97e75430dcc4072c713dff2b2361
Alex Schwartz <aschwartz@detaso.com>
2021-04-03 17:03:07 -0500

    Add all with limit method to merchant

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -8,4 +8,25 @@ RSpec.describe Merchant, type: :model do
     it { should have_many(:customers).through(:invoices) }
     it { should have_many(:transactions).through(:invoices) }
   end
+
+  describe 'class methods' do
+    describe '::all_limit' do
+      it 'returns <limit> merchants' do
+        merchants = create_list(:merchant, 10)
+
+        expect(Merchant.all_limit(5).size).to eq(5)
+      end
+
+      it 'returns empty array when limit < 1' do
+        merchants = create_list(:merchant, 10)
+
+        expect(Merchant.all_limit(0).size).to eq(0)
+        expect(Merchant.all_limit(-1).size).to eq(0)
+      end
+
+      it 'returns empty array when there are no merchants' do
+        expect(Merchant.all_limit(5).size).to eq(0)
+      end
+    end
+  end
 end
