f089c5eac93c6f521cacde90d930f147e6756623
Alex Schwartz <aschwartz@detaso.com>
2021-04-03 13:39:26 -0500

    Add per_page and page_number helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+  def per_page
+    @per_page ||= params[:per_page] || 20
+  end
+
+  def page_number
+    @page_number ||= params[:page] || 1
+  end
+
+  helper_method :params_per_page, :params_page_number
 end
