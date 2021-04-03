bce56c3fb4e28477f6690d5f493601372e4a8956
Alex Schwartz <aschwartz@detaso.com>
2021-04-03 17:03:36 -0500

    Update page helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::API
   def per_page
-    @per_page ||= params[:per_page] || 20
+    @per_page = params[:per_page] ? params[:per_page].to_i : 20
   end
 
   def page_number
-    @page_number ||= params[:page] || 1
+    @page_number = params[:page] ? params[:page].to_i : 1
   end
 
   helper_method :params_per_page, :params_page_number
