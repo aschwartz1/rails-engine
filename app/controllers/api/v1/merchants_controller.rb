52881d513ec867f305e406d5ebff8a877282752f
Alex Schwartz <aschwartz@detaso.com>
2021-04-04 18:49:36 -0500

    Update json response, postman spec now passes

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,24 +1,50 @@
 class Api::V1::MerchantsController < ApplicationController
   def index
     # render json: Merchant.all
-    render json: (paginate(limited_merchants))
+    render json: all_merchants_response
   end
 
   private
 
+  def all_merchants_response
+    {
+      data: format_merchants(paginate(all_merchants))
+    }
+  end
+
+  def format_merchants(merchants)
+    merchants.map do |merchant|
+      {
+        id: merchant.id.to_s,
+        type: 'merchant',
+        attributes: merchant_attributes(merchant)
+      }
+    end
+  end
+
+  def merchant_attributes(merchant)
+    {
+      name: merchant.name
+    }
+  end
+
   def paginate(results)
     last_results_index = (results.size - 1)
     start_index = per_page * (page_number - 1)
-    return [] if start_index > last_results_index
     end_index = minimum(last_results_index, (records_needed_for_request - 1))
-    results[start_index..end_index]
+
+    if start_index > last_results_index
+      []
+    else
+      results[start_index..end_index]
+    end
   end
 
   def records_needed_for_request
     page_number * per_page
   end
 
-  def limited_merchants
+  def all_merchants
     Merchant.all_limit(records_needed_for_request)
   end
 
--- spec/requests/api/v1/merchants/get_all_request_spec.rb
+++ spec/requests/api/v1/merchants/get_all_request_spec.rb
@@ -1,20 +1,49 @@
 require 'rails_helper'
 
 describe 'Get all merchants request' do
-  it 'sends a list of merchants' do
-    create_list(:merchant, 3)
-    get api_v1_merchants_path
+  describe 'structure' do
+    it 'main response body contains one key' do
+      get api_v1_merchants_path
 
-    expect(response).to be_successful
-    merchants = JSON.parse(response.body, symbolize_names: true)
+      expect(response).to be_successful
+      body = JSON.parse(response.body, symbolize_names: true)
 
-    expect(merchants.count).to eq(3)
-    merchants.each do |merchant|
-      expect(merchant).to have_key(:id)
-      expect(merchant[:id]).to be_an(Integer)
+      expect(body.size).to eq(1)
+      expect(body).to have_key(:data)
+      expect(body[:data]).to be_an(Array)
+      expect(body[:data].size).to eq(0)
+    end
 
-      expect(merchant).to have_key(:name)
-      expect(merchant[:name]).to be_a(String)
+    it 'body[:data] is an array of merchants' do
+      create_list(:merchant, 5)
+      get api_v1_merchants_path
+
+      expect(response).to be_successful
+      body = JSON.parse(response.body, symbolize_names: true)
+      merchants = body[:data]
+      actual_merchants = Merchant.all
+
+      # Assert that each merchant representation is correct
+      merchants.each_with_index do |merchant, i|
+        expect(merchant.keys.size).to eq(3)
+
+        expect(merchant).to have_key(:id)
+        expect(merchant[:id]).to be_a(String)
+        expect(merchant[:id]).to eq(actual_merchants[i].id.to_s)
+
+        expect(merchant).to have_key(:type)
+        expect(merchant[:type]).to be_a(String)
+        expect(merchant[:type]).to eq('merchant')
+
+        expect(merchant).to have_key(:attributes)
+        expect(merchant[:attributes]).to be_a(Hash)
+        expect(merchant[:attributes].size).to eq(1)
+
+        attributes = merchant[:attributes]
+        expect(attributes).to have_key(:name)
+        expect(attributes[:name]).to be_a(String)
+        expect(attributes[:name]).to eq(actual_merchants[i].name)
+      end
     end
   end
 
@@ -24,9 +53,10 @@ describe 'Get all merchants request' do
       get api_v1_merchants_path
 
       expect(response).to be_successful
-      merchants = JSON.parse(response.body, symbolize_names: true)
+      body = JSON.parse(response.body, symbolize_names: true)
+      merchants = body[:data]
 
-      expect(merchants.count).to eq(20)
+      expect(merchants.size).to eq(20)
     end
 
     it 'defaults to page 1' do
@@ -38,10 +68,11 @@ describe 'Get all merchants request' do
       expected_last_merchant = all_merchants[19]
 
       expect(response).to be_successful
-      actual_merchants = JSON.parse(response.body, symbolize_names: true)
+      body = JSON.parse(response.body, symbolize_names: true)
+      actual_merchants = body[:data]
       expect(actual_merchants.size).to eq(20)
-      expect(actual_merchants.first[:id]).to eq(expected_first_merchant.id)
-      expect(actual_merchants.last[:id]).to eq(expected_last_merchant.id)
+      expect(actual_merchants.first[:id]).to eq(expected_first_merchant.id.to_s)
+      expect(actual_merchants.last[:id]).to eq(expected_last_merchant.id.to_s)
     end
   end
 
@@ -55,10 +86,11 @@ describe 'Get all merchants request' do
       expected_last_merchant = all_merchants[1]
 
       expect(response).to be_successful
-      actual_merchants = JSON.parse(response.body, symbolize_names: true)
+      body = JSON.parse(response.body, symbolize_names: true)
+      actual_merchants = body[:data]
       expect(actual_merchants.size).to eq(2)
-      expect(actual_merchants.first[:id]).to eq(expected_first_merchant.id)
-      expect(actual_merchants.last[:id]).to eq(expected_last_merchant.id)
+      expect(actual_merchants.first[:id]).to eq(expected_first_merchant.id.to_s)
+      expect(actual_merchants.last[:id]).to eq(expected_last_merchant.id.to_s)
     end
 
     it 'returns correct page of data when page is specified' do
@@ -70,10 +102,11 @@ describe 'Get all merchants request' do
       expected_last_merchant = all_merchants[39]
 
       expect(response).to be_successful
-      actual_merchants = JSON.parse(response.body, symbolize_names: true)
+      body = JSON.parse(response.body, symbolize_names: true)
+      actual_merchants = body[:data]
       expect(actual_merchants.size).to eq(20)
-      expect(actual_merchants.first[:id]).to eq(expected_first_merchant.id)
-      expect(actual_merchants.last[:id]).to eq(expected_last_merchant.id)
+      expect(actual_merchants.first[:id]).to eq(expected_first_merchant.id.to_s)
+      expect(actual_merchants.last[:id]).to eq(expected_last_merchant.id.to_s)
     end
 
     it 'correct number and page when per_page and page are specified' do
@@ -85,10 +118,11 @@ describe 'Get all merchants request' do
       expected_last_merchant = all_merchants[3]
 
       expect(response).to be_successful
-      actual_merchants = JSON.parse(response.body, symbolize_names: true)
+      body = JSON.parse(response.body, symbolize_names: true)
+      actual_merchants = body[:data]
       expect(actual_merchants.size).to eq(2)
-      expect(actual_merchants.first[:id]).to eq(expected_first_merchant.id)
-      expect(actual_merchants.last[:id]).to eq(expected_last_merchant.id)
+      expect(actual_merchants.first[:id]).to eq(expected_first_merchant.id.to_s)
+      expect(actual_merchants.last[:id]).to eq(expected_last_merchant.id.to_s)
     end
 
   end
@@ -104,24 +138,34 @@ describe 'Get all merchants request' do
       expected_last_merchant = all_merchants[14]
 
       expect(response).to be_successful
-      actual_merchants = JSON.parse(response.body, symbolize_names: true)
+      body = JSON.parse(response.body, symbolize_names: true)
+      actual_merchants = body[:data]
       expect(actual_merchants.size).to eq(5)
-      expect(actual_merchants.first[:id]).to eq(expected_first_merchant.id)
-      expect(actual_merchants.last[:id]).to eq(expected_last_merchant.id)
+      expect(actual_merchants.first[:id]).to eq(expected_first_merchant.id.to_s)
+      expect(actual_merchants.last[:id]).to eq(expected_last_merchant.id.to_s)
     end
 
     it 'sends empty array when there are no merchants' do
       get api_v1_merchants_path
 
       expect(response).to be_successful
-      merchants = JSON.parse(response.body, symbolize_names: true)
+      body = JSON.parse(response.body, symbolize_names: true)
+      merchants = body[:data]
 
       expect(merchants).to be_an(Array)
-      expect(merchants.count).to eq(0)
+      expect(merchants.size).to eq(0)
     end
 
-    it 'sends page 1 if page number of <= 0' do
+    it 'sends page 1 if page number <= 0' do
+      create_list(:merchant, 10)
+      get api_v1_merchants_path(page: 0)
 
+      expect(response).to be_successful
+      body = JSON.parse(response.body, symbolize_names: true)
+      merchants = body[:data]
+
+      expect(merchants).to be_an(Array)
+      expect(merchants.size).to eq(10)
     end
 
     it 'sends empty array when page_number is past the # of pages in the db' do
@@ -130,7 +174,8 @@ describe 'Get all merchants request' do
       get api_v1_merchants_path(page: 200)
 
       expect(response).to be_successful
-      merchants = JSON.parse(response.body, symbolize_names: true)
+      body = JSON.parse(response.body, symbolize_names: true)
+      merchants = body[:data]
 
       expect(merchants).to be_an(Array)
       expect(merchants.size).to eq(0)
