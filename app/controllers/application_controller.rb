10e419a3f9437f5190afcedb0fda0ef96f7f42e7
Alex Schwartz <aschwartz@detaso.com>
2021-04-04 18:49:04 -0500

    Refactor pagination helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,33 @@ class ApplicationController < ActionController::API
   include ActionController::Helpers
 
   def per_page
-    @per_page = params[:per_page] ? params[:per_page].to_i : 20
+    @per_page ||= fetch_per_page
   end
 
   def page_number
-    @page_number = params[:page] ? params[:page].to_i : 1
+    @page_number ||= fetch_page_number
+  end
+
+  private
+
+  def fetch_per_page
+    per_page_param = params[:per_page].to_i
+
+    if per_page_param.positive?
+      per_page_param
+    else
+      20
+    end
+  end
+
+  def fetch_page_number
+    page_param = params[:page].to_i
+
+    if page_param.positive?
+      page_param
+    else
+      1
+    end
   end
 
   helper_method :per_page, :page_number
