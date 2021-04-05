74744ac850f726b58219e4d415880a05e1d0bf3a
Alex Schwartz <aschwartz@detaso.com>
2021-04-04 19:49:05 -0500

    Add endpoint for fetching single merchant

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -4,6 +4,16 @@ class Api::V1::MerchantsController < ApplicationController
     render json: all_merchants_response
   end
 
+  def show
+    id_param = params[:id]
+
+    if Merchant.exists?(id_param)
+      render json: single_merchant_response(id_param)
+    else
+      render json: not_found, status: :not_found
+    end
+  end
+
   private
 
   def all_merchants_response
@@ -14,17 +24,34 @@ class Api::V1::MerchantsController < ApplicationController
 
   def format_merchants(merchants)
     merchants.map do |merchant|
-      {
-        id: merchant.id.to_s,
-        type: 'merchant',
-        attributes: merchant_attributes(merchant)
-      }
+      format_merchant(merchant)
     end
   end
 
   def merchant_attributes(merchant)
     {
       name: merchant.name
+    }
+  end
+
+  def single_merchant_response(id)
+    {
+      data: format_merchant(Merchant.find(id))
+    }
+  end
+
+  def format_merchant(merchant)
+    {
+      id: merchant.id.to_s,
+      type: 'merchant',
+      attributes: merchant_attributes(merchant)
+    }
+  end
+
+  def not_found
+    {
+      code: 404,
+      status: 'Not Found'
     }
   end
 
