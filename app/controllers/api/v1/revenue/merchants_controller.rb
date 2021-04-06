a4bf31ac35edfd90cbe135b0fb5746db284fd342
Alex Schwartz <aschwartz@detaso.com>
2021-04-06 17:36:43 -0500

    Refactor merchant revenue for happy & sad path

--- app/controllers/api/v1/revenue/merchants_controller.rb
+++ app/controllers/api/v1/revenue/merchants_controller.rb
@@ -1,8 +1,13 @@
 class Api::V1::Revenue::MerchantsController < ApplicationController
   def show
     merchant_id = params[:id]
-    merchant = Merchant.find(merchant_id)
 
-    render json: MerchantRevenueSerializer.new(merchant)
+    if Merchant.exists?(merchant_id)
+      merchant = Merchant.find(merchant_id)
+      render json: MerchantRevenueSerializer.new(merchant)
+    else
+      merchant = OpenStruct.new({ id: nil, total_revenue: nil })
+      render json: MerchantRevenueSerializer.new(merchant), status: :not_found
+    end
   end
 end
