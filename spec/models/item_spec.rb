73bfa70b9c19bb1814f85e3bb3bff2f63d4356c1
Alex Schwartz <aschwartz@detaso.com>
2021-04-07 08:46:22 -0500

    Add all with limit query to item

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -7,4 +7,25 @@ RSpec.describe Item, type: :model do
     it { should have_many(:invoices).through(:invoice_items) }
     it { should have_many(:customers).through(:invoices) }
   end
+
+  describe 'class methods' do
+    describe '::all_limit' do
+      it 'returns <limit> items' do
+        merchants = create_list(:item, 10)
+
+        expect(Item.all_limit(5).size).to eq(5)
+      end
+
+      it 'returns empty array when limit < 1' do
+        items = create_list(:item, 10)
+
+        expect(Item.all_limit(0).size).to eq(0)
+        expect(Item.all_limit(-1).size).to eq(0)
+      end
+
+      it 'returns empty array when there are no merchants' do
+        expect(Item.all_limit(5).size).to eq(0)
+      end
+    end
+  end
 end
