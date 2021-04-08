58a70053439d94f4a3de38ade99067dc85a375e5
Alex Schwartz <aschwartz@detaso.com>
2021-04-08 09:47:58 -0500

    Remove method

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,20 +9,4 @@ class Item < ApplicationRecord
 
     Item.all.limit(limit)
   end
-
-  def self.destroy_consider_invoice(id)
-    item = Item.find(id)
-    invoices = item
-                .invoices.includes(:invoice_items)
-
-    require "pry"; binding.pry
-    destroy(id)
-    require "pry"; binding.pry
-
-    invoices.each do |invoice|
-      i = invoice.invoice_items
-      require "pry"; binding.pry
-      Invoice.destroy(invoice.id) if invoice.invoice_items.empty?
-    end
-  end
 end
